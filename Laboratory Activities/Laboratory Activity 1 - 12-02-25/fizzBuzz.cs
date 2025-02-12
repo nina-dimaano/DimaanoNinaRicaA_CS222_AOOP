58d733a17581acf544ebae215761ac89fcabbbe8
Nina Rica Dimaano <165743107+nina-dimaano@users.noreply.github.com>
2025-02-12 17:53:36 +0800

    Update fizzBuzz.cs

--- Laboratory Activities/Laboratory Activity 1 - 12-02-25/fizzBuzz.cs
+++ Laboratory Activities/Laboratory Activity 1 - 12-02-25/fizzBuzz.cs
@@ -1,20 +1,26 @@
 
 using System;
 
-class fizzBuzz {
+class fizzBuzz 
+{
     static void Main()
     {
-        for (int i = 1; i <= 15; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        for (int i = 1; i <= 15; i++) 
+        {
+            if (i % 3 == 0 && i % 5 == 0) 
+            {
                 Console.WriteLine("FizzBuzz");
             }
-            else if(i % 3 == 0) {
+            else if(i % 3 == 0) 
+            {
                 Console.WriteLine("Fizz");
             }
-            else if (i % 5 == 0) {
+            else if (i % 5 == 0) 
+            {
                 Console.WriteLine("Buzz");
             }
-            else {
+            else 
+            {
                 Console.WriteLine($"{i}");
             }
         }
